7acfb998e4f0fcf8cc461c7b4e6ad56df81dcef7
wilber4c <nsengiyumvawilberforce@gmail.com>
2023-07-11 10:02:57 +0300

    font removed

--- resources/views/cvgenerator/cv.blade.php
+++ resources/views/cvgenerator/cv.blade.php
@@ -51,11 +51,6 @@
             border-radius: 50%;
         }
     </style>
-        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
