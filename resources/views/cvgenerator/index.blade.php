c2de7f6bd2f9666f17845ddf48b0b2e41dfc2a32
wilber4c <nsengiyumvawilberforce@gmail.com>
2023-07-25 05:51:59 +0300

    cv section updated

--- resources/views/cvgenerator/index.blade.php
+++ resources/views/cvgenerator/index.blade.php
@@ -8,19 +8,19 @@
                     potential with our free resume templates and easy-to-use design editor</p>
             </div>
             <div class="row">
-                <div class="col-md-3 card card-body m-1 shadow w-25">
+                <div class="col-md-3 card card-body m-1 shadow col-sm-12">
                     <a href="/cv-generator/edit-cv/international">
                         <img src="{{ asset('assets/images/international_template.png') }}" alt="cv2 placeholder" class="img-fluid">
                     </a>
                     <h3>International</h3>
                 </div>
-                <div class="col-md-3 card card-body m-1 shadow w-25">
+                <div class="col-md-3 card card-body m-1 shadow col-sm-12">
                     <a href="/cv-generator/edit-cv/modern">
                         <img src="{{ asset('assets/images/modern_template.png') }}" alt="cv3 placeholder" class="img-fluid">
                     </a>
                     <h3>Modern</h3>
                 </div>
-                <div class="col-md-3 card card-body m-1 shadow w-25">
+                <div class="col-md-3 card card-body m-1 shadow col-sm-12">
                     <a href="/cv-generator/edit-cv/classic">
                         <img src="{{ asset('assets/images/classic_template.png') }}" alt="cv4 placeholder" class="img-fluid">
                     </a>
