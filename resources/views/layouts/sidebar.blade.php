9a41da38d751754c35eaad9d8ee4fafb97acffc4
wilber4c <nsengiyumvawilberforce@gmail.com>
2023-07-25 11:36:00 +0300

    templates updates

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,9 +26,9 @@
     </div>
 
     <div class="pxp-dashboard-side-panel d-none d-lg-block">
-        <div class="pxp-logo">
+        <a href="/" class="pxp-logo">
             <img src="{{ asset('assets/images/logo.png') }}" alt="logo">
-        </div>
+        </a>
 
         <nav class="mt-3 mt-lg-4 d-flex justify-content-between flex-column pb-100">
             <div class="pxp-dashboard-side-label">Admin tools</div>
